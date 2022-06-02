d4ecdf9bdd7f4633a584c9be90dcb19c805f05c6
Nisar Ahmad <nisarahmad2017a@gmail.com>
2022-06-02 16:03:19 +0500

    changed a  file add

---
+++ dbConfig.js
@@ -0,0 +1,12 @@
+
+   const dbConfig = {
+        user: "admin", // DB Server Login
+        password: "A@Zmet@l!!123", // DB Server Password
+      //server: "72.255.34.92", // DB Server Server name or IP
+        server:'119.153.105.230',
+        database: "satrader",  // Database Name
+        options:  {
+        encrypt: false
+          }
+    }
+module.exports = dbConfig ;
